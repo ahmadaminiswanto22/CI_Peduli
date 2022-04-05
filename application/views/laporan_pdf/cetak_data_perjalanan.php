8728921cac89dd6b2aa731be8f988d2d0abc8d3e
ahmadaminiswanto22 <ahmadaminiswanto@gmail.com>
2022-04-05 11:13:39 +0700

    nama halaman PDF

--- application/views/laporan_pdf/cetak_data_perjalanan.php
+++ application/views/laporan_pdf/cetak_data_perjalanan.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title_pdf; ?></title>
+    <!-- Favicons -->
+    <link href="<?= base_url() ?>assets/img/logo.png" rel="icon">
+    <link href="<?= base_url() ?>assets/img/logo.png" rel="apple-touch-icon">
+
+    <title>Cetak PDF</title>
     <style>
         #table {
             font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
